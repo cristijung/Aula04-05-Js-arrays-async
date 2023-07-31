0a03a8e0248460f5c1a73bf002badd2faba361af
Cristina Jung <cristijung@gmail.com>
2023-07-31 17:45:57 -0300

    ajuste api

--- Aula05/async/api-dog/script-api.js
+++ Aula05/async/api-dog/script-api.js
@@ -1,24 +1,26 @@
-const pesquisaRaca = async () => {
+
+const pesquisarRaca = async () => {
     let raca = document.getElementById('raca').value;
 
     if (raca.trim() === "") {
-        document.getElementById('resposta').innerHTML = `<h5>O campo raça deve ser preenchido</h5>`        
+        document.getElementById('resposta').innerHTML = `<h5>O campo raça deve ser preenchido</h5>`
     } else {
         await fetch(`https://api.thedogapi.com/v1/breeds/search?q=${raca}`, {
-        method: 'GET',
-        headers: {
-            'Content-type': 'application/json'
-        }
-    }).then((response) => {
-        if (!response.ok) {
-            throw new Error(`HTTP error: ${response.status}`);
-        }
-
-        return response.json();
-    }).then((dogs) => {
-        if (dogs.length === 0) {
-            document.getElementById('resposta').innerHTML = 
-            `<div class="card" style="width: 18rem;">
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`);
+            }
+            return response.json();
+        }).then((dogs) => {
+            if (dogs.length === 0) {
+                document.getElementById('resposta').innerHTML = `<h5>Raça não encontrada</h5>`
+            } else {
+                document.getElementById('resposta').innerHTML =
+                    `<div class="card" style="width: 18rem;">
                         <div class="card-body">
                             <h5 class="card-title">${dogs[0]['name']}</h5>
                         </div>
@@ -28,7 +30,7 @@ const pesquisaRaca = async () => {
                             <li class="list-group-item"><b>Temperamento:</b> ${dogs[0]['temperament']}</li>
                         </ul>
                     </div><br>`
-        }
-    })
+            }
+        })
     }
 };
